2002a517db3961cba1af9e9f7280e257979dd923
Yastrzhemska <yastrzhemska@gmail.com>
2023-06-15 16:06:13 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,10 @@
-const numberOofCategories = categories => {
-    let totalNumber = 0;
-    categories.forEach((item) => {
-totalNumber =+ item
-    });
-}
-console.log(totalNumber);
 
+
+const itemEl = document.querySelectorAll('.item');
+console.log('Number of categories:', itemEl.length);
+
+itemEl.forEach(function (item) {
+    console.log('Category:', item.children[0].textContent);
+    console.log('Elements:', item.children[1].children.length);
+});
 
