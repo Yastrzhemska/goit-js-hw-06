ea610fd7ccf4be9da5b79a1ee94c8367ceda7312
Yastrzhemska <yastrzhemska@gmail.com>
2023-06-15 20:29:29 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -12,42 +12,10 @@ const ingredients = [
 const productsContainerEl = document.querySelector('#ingredients')
 
 
-const li = document.createElement('li');
-li.textContent = 'Potatoes';
-li.classList.add('item');
-
-console.log(li);
-
-const li2 = document.createElement('li');
-li2.textContent = 'Mushrooms';
-li2.classList.add('item');
-
-console.log(li2);
-
-const li3 = document.createElement('li');
-li3.textContent = 'Garlic';
-li3.classList.add('item');
-
-console.log(li3);
-
-const li4 = document.createElement('li');
-li4.textContent = 'Tomatos';
-li4.classList.add('item');
-
-console.log(li4);
-
-const li5 = document.createElement('li');
-li5.textContent = 'Herbs';
-li5.classList.add('item');
-
-console.log(li5);
-
-const li6 = document.createElement('li');
-li6.textContent = 'Condiments';
-li6.classList.add('item');
-
-console.log(li6);
-
-const liItem = [li, li2, li3, li4, li5, li6];
-
-productsContainerEl.append(...liItem);
+const productsItem = ingredients.map(name =>
+  `<li class="item">
+  ${name}
+  </li>`).join('')
+  
+console.log(productsItem);
+productsContainerEl.insertAdjacentHTML('afterbegin', productsItem)
