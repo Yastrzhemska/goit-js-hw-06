b116ef0d6109cc293dcde5bea1cc036973c8dd81
Yastrzhemska <yastrzhemska@gmail.com>
2023-06-20 18:01:38 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+console.log(button);
+
+const span = document.querySelector('.color');
+console.log(span);
+
+const body = document.querySelector('body');
+console.log(body);
+
+button.addEventListener('click', changeColorHandler);
+
+function changeColorHandler() {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = body.style.backgroundColor;
+}
